d316ab01d60848373bdd5e69b71198b9d7bc2804
West Gore <boringmatter@gmail.com>
2022-06-25 17:04:50 -0400

    java spring boot changes

---
+++ spring-boot_Angular_Reddit/spring-reddit-clone/src/main/java/com/example/demo/com.example.springredditclone/model/Comment.java
@@ -0,0 +1,35 @@
+package com.example.demo.com.example.springredditclone.model;
+
+import lombok.Data;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+import org.apache.catalina.User;
+
+import java.time.Instant;
+
+import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    @NotEmpty
+    private String text;
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "postId", referencedColumnName = "postId")
+    private Post post;
+    private Instant createdDate;
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private User user;
+}
